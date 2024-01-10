7dadbd426fff03ecc94a32b79bf03aae1a4898ef
rygoto <ryudono08@gmail.com>
2024-01-10 18:59:49 +0900

    countryname

--- src/server/FetchvideoServer.js
+++ src/server/FetchvideoServer.js
@@ -9,7 +9,7 @@ const port = 3000;
 async function fetchVideos(regionCode) {
     const youtube = google.youtube({
         version: 'v3',
-        auth: 'AIzaSyAttX4v8wYrjQVo2yeqzSXWLBjfrRu3KZY' // ここにあなたのYouTube APIキーを設定
+        auth: ''
     });
 
     const response = await youtube.videos.list({
